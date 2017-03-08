63859b9779698dbae130dadb47d48a000c0401b4
derick alfaro <derickalfaro@gmail.com>
2017-03-07 21:24:45 -0500

    almost done setting up

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,15 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const uglify = require('gulp-uglify')
 const pump = require('pump')
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
+const eslint = require('gulp-eslint')
 
 gulp.task('sass', () => {
   let input = './src/assets/sass/style.sass'
   let output = './build/assets/css'
   return gulp.src(input)
     .pipe(sass())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
     .pipe(gulp.dest(output))
 })
 
@@ -23,6 +19,14 @@ gulp.task('sass:watch', () => {
   gulp.watch('./src/assets/sass/**/*.sass', ['sass']);
 })
 
+gulp.task('css', () => {
+  gulp.src('src/app.css')
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+      }))
+      .pipe(gulp.dest('dist'))
+})
 
 gulp.task('compress', cb => {
   pump([
@@ -34,10 +38,28 @@ gulp.task('compress', cb => {
   )
 })
 
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('html', () => {
+  gulp.src('./src/**/*.html')
+  .pipe(gulp.dest('./build'))
+})
+
 gulp.task('img', () =>
-    gulp.src('src/assets/img/*')
+    gulp.src('./src/assets/img/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('build/img'))
+        .pipe(gulp.dest('./build/assets/img'))
 );
 
-gulp.task('default',['sass', 'sass:watch', 'img', 'compress'])
+gulp.task('default',['sass', 'sass:watch', 'img', 'compress', 'html', 'lint'])
