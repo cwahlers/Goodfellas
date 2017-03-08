e5f5791deb90f9b6c5c7b6de74865c035443fa8f
derick alfaro <derickalfaro@gmail.com>
2017-03-07 21:01:21 -0500

    more updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,43 @@
 'use strict';
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const pump = require('pump')
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+
 
 gulp.task('sass', () => {
-  var input = './src/assets/sass/style.sass'
-  var output = './build/assets/css/'
+  let input = './src/assets/sass/style.sass'
+  let output = './build/assets/css'
   return gulp.src(input)
     .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest(output))
-});
+})
 
 gulp.task('sass:watch', () => {
   gulp.watch('./src/assets/sass/**/*.sass', ['sass']);
-});
+})
 
-gulp.task('default',['sass', 'sass:watch'])
+
+gulp.task('compress', cb => {
+  pump([
+      gulp.src('./src/js/**/*.js'),
+      uglify(),
+      gulp.dest('./build/assets/js')
+    ],
+    cb
+  )
+})
+
+gulp.task('img', () =>
+    gulp.src('src/assets/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'))
+);
+
+gulp.task('default',['sass', 'sass:watch', 'img', 'compress'])
