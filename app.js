451e41bbc24e0a316d0ec2b6826024c92b785335
derick alfaro <derickalfaro@gmail.com>
2017-03-09 00:21:38 -0500

    update

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 'use strict'
-var express = require('express')
-var exphbs  = require('express-handlebars')
-var bodyParser = require('body-parser')
-var passport = require('passport') //user auth
-var morgan = require('morgan')
-var path = require('path')
-//var mysql = require('mysql')
-
-var routes = require('./routes/index.js')
+var express         = require('express')
+var exphbs          = require('express-handlebars')
+var bodyParser      = require('body-parser')
+var cookieParser    = require('cookie-parser')
+var session         = require('express-session')
+var passport        = require('passport')
+var LocalStrategy   = require('passport-local').Strategy
+var morgan          = require('morgan')
+var flash           = require('connect-flash')
+var path            = require('path')
+var mysql           = require('mysql')
+var port            = process.env.PORT || 8080
+var routes          = require('./routes/index.js')
 
 var app = express()
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.use(morgan('dev'))
+app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, '/build')))
-// app.use(session({
-//   secret: 'goodfellas',
-//   resave: true,
-//   saveUninitialized: true
-// }))
+
+app.use(session({
+  secret: 'goodfellas',
+  resave: true,
+  saveUninitialized: true
+}))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(flash())
 
 app.use('/', routes)
 
-app.listen(3000)
+app.listen(port)
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,43 @@
 var express = require('express')
+var passport = require('passport')
 var router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Home', css:['style.css'], js:['main.js']})
+    res.render('home', {
+        title: 'Home',
+        css: ['style.css'],
+        js: ['main.js']
+    })
 })
 
-router.get('/login', (req, res) => {
-    res.render('login', {title: 'Login', css:['login.css'], js:['main.js']})
+router.post('/login', passport.authenticate('local', {
+    failureFlash: 'Invalid username or password.',
+    successFlash: 'Welcome!'
+}), (req, res) => {
+    res.redirect('/users/' + req.user.username)
 })
 
 router.get('/portal', (req, res) => {
-    res.render('portal', {title: 'Portal', css:['portal.css'], js:['main.js']})
+    res.render('portal', {
+        title: 'Portal',
+        css: ['portal.css'],
+        js: ['main.js']
+    })
 })
 
-router.post('/register', (req, res) => {
-    res.render('register', {title: 'Register', css:['register.css'], js:['main.js']})
+router.post('/register')
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Register',
+        css: ['register.css'],
+        js: ['main.js']
+    })
 })
 
+router.get('/logout', (req, res) => {
+		req.logout();
+		res.redirect('/');
+	})
 
 module.exports = router
